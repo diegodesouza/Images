a587425b99d2340c059702537fc7f84eb23326dd
Diego De Souza <diegotrus7@gmail.com>
2015-02-02 22:53:38 -0500

    rearranged the index page added show on scroll added footer and top bar added social media styling done

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
 
-  $(".circle.circle-left").scroll();
-
+// show on scroll
+$(window).scroll(function() {
+    $("img").each( function() {
+        if( $(window).scrollTop() > $(this).offset().top - 370) {
+            $(this).css('opacity', 1);
+        } else {
+            $(this).css('opacity', 0);
+        }
+    });
 });

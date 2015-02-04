93129b7b59e85dc2024a53059242a65a4e1bbaba
Diego De Souza <diegotrus7@gmail.com>
2015-02-04 00:36:58 -0500

    more styling done on index and show page styled fit smaller screens

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,7 +1,7 @@
 // show on scroll
 $(window).scroll(function() {
     $("img").each(function() {
-        if( $(window).scrollTop() > $(this).offset().top - 370) {
+        if( $(window).scrollTop() > $(this).offset().top - 390) {
             $(this).css('opacity', 1);
         } else {
             $(this).css('opacity', 0);
@@ -11,7 +11,7 @@ $(window).scroll(function() {
 
 $(window).scroll(function() {
     $("h3").each(function() {
-        if( $(window).scrollTop() > $(this).offset().top - 500) {
+        if( $(window).scrollTop() > $(this).offset().top - 550) {
             $(this).css('opacity', 1);
         } else {
             $(this).css('opacity', 0);
